6f9bb4bcd7d43eef18635b7260c41311d1fe3c46
mhulin <michelle.hulin@emr.ac.uk>
2018-02-06 15:03:54 +0000

    Create sub_bayestraits_effectors_mcmc.sh

---
+++ bayestraits/sub_bayestraits_effectors_mcmc.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -pe smp 1
+#$ -l virtual_free=1G
+#$ -l h=blacklace06.blacklace|blacklace07.blacklace|blacklace08.blacklace|blacklace09.blacklace|blacklace10.blacklace
+
+Usage='sub_bayestraits.sh <no_replicates> <directory> <tree>'
+
+i=$1
+d=$2
+t=$3
+
+for EFFECTOR in "$d"/txt_files/*_"$i".txt ; do
+EFFECTOR_FILE=$(basename $EFFECTOR)
+EFFECTOR_SHORT=$(echo $EFFECTOR_FILE | sed s/.txt//g)
+
+echo $i
+
+
+COMMANDS=/home/hulinm/pseudomonas_data/pseudomonas/analysis/bayestraits/new_analysis/independant_commands_mcmc.txt
+
+BayesTraitsV3 $t $EFFECTOR <$COMMANDS
+
+
+mv "$d"/txt_files/"$EFFECTOR_SHORT".txt.log.txt  "$d"/MLoutput/"$i"/"$EFFECTOR_SHORT"_independant
+
+# Running of bayestraits with phylogenetic tree and effector file for discrete dependant analysis
+
+COMMANDS=/home/hulinm/pseudomonas_data/pseudomonas/analysis/bayestraits/new_analysis/dependant_commands_mcmc.txt
+
+BayesTraitsV3 $t $EFFECTOR <$COMMANDS
+mv "$d"/txt_files/"$EFFECTOR_SHORT".txt.log.txt  "$d"/MLoutput/"$i"/"$EFFECTOR_SHORT"_dependant
+
+done
+done
+
+
