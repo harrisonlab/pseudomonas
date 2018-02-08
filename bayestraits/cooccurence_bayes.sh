670a0a0919be30cd97f33a83044eca3258c6cb8c
hulinm <michelle.hulin@emr.ac.uk>
2018-02-08 16:42:30 +0000

    Added commands to run bayestraits

--- bayestraits/cooccurence_bayes.sh
+++ bayestraits/cooccurence_bayes.sh
@@ -20,19 +20,38 @@ rm !(*_*)
 
 
 # Make files compatible with bayestraits
-
-for i in `seq 1 100`; do            
-#mkdir $i # For making directory within MLoutput folder
+# Currently working in data/scratch folder due to cluster problems 
+for i in `seq 1 100`; do         
+echo $i   
+mkdir $i # For making directory within MLoutput folder
 for EFFECTOR in /home/hulinm/pseudomonas_data/pseudomonas/analysis/bayestraits/cooccurence/files/*.txt2 ; do
 EFFECTOR_FILE=$(basename $EFFECTOR)
 EFFECTOR_SHORT=$(echo $EFFECTOR_FILE | sed s/.txt2//g)
-cp "$EFFECTOR_SHORT".txt2 "$EFFECTOR_SHORT"_"$i".txt
+cp "$EFFECTOR_SHORT".txt2 /data/scratch/hulinm/"$EFFECTOR_SHORT"_"$i".txt
 
 
-for EFFECTOR in /home/hulinm/pseudomonas_data/pseudomonas/analysis/bayestraits/cooccurence/files/*.txt ; do
+
+for EFFECTOR in /data/scratch/hulinm/*.txt ; do
 EFFECTOR_FILE=$(basename $EFFECTOR)
 EFFECTOR_SHORT=$(echo $EFFECTOR_FILE | sed s/.txt//g)
+echo $EFFECTOR_SHORT
 cat $EFFECTOR | sed 1d > $EFFECTOR_SHORT # removing header line
+
+
+for i in `seq 1 100`; do             
+for EFFECTOR in /data/scratch/hulinm/*_"$i" ; do
+EFFECTOR_FILE=$(basename $EFFECTOR)
+echo $EFFECTOR_FILE
+mv $EFFECTOR_FILE "$EFFECTOR_FILE".txt
+
+
+# Run BayestraitsV2
+
+
+for i in `seq 3 50`; do
+qsub /home/hulinm/git_repos/pseudomonas/bayestraits/sub_bayestraits_effectorsV2.sh $i /data/scratch/hulinm/cooccurence /home/hulinm/pseudomonas_data/pseudomonas/analysis/bayestraits/final/tree.nex
+done
+
 
 
 
